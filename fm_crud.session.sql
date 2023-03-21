fcc49d6adcbd0a6bcb217bdf87949016713e1151
Evgeniy Selivanov <genyaselivanovzp@gmail.com>
2023-03-21 11:04:06 +0200

    add server & connect db

---
+++ fm_crud.session.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "things"(
+  "id" bigserial PRIMARY KEY,
+  "body" text NOT NULL CHECK("body"!=''),
+  "updatedAt" timestamp NOT NULL DEFAULT current_timestamp,
+  "createdAt" timestamp NOT NULL DEFAULT current_timestamp
+);
+
---
+++ models/Thing.js
@@ -0,0 +1,13 @@
+class Thing {
+  static client = null;
+  static tablename="things";
+
+  static async create() { }
+  static async findAll() { }
+  static async findByPk() { }
+  static async updateByPk() { }
+  static async deleteByPk() { }
+}
+
+
+module.exports = Thing;
